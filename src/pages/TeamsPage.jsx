7b63acbc47e66269eb18dc850ea8f4a914c26a62
Dmitry Umnov <umnov.dm@gmail.com>
2023-03-06 21:57:58 +0400

    generate all teams

--- src/pages/TeamsPage.jsx
+++ src/pages/TeamsPage.jsx
@@ -19,77 +19,71 @@ const ulyanovskGirls = [
 const allGirls = [...armenianGirls, ...ulyanovskGirls].sort();
 
 const TeamsPage = () => {
-  const [remainingGirls, setRemainingGirls] = useState(allGirls);
   const [teams, setTeams] = useState([]);
-  const [armenianSelected, setArmenianSelected] = useState(false);
 
-  function generateTeam() {
-    if (!armenianSelected) {
-      setTeams([[...armenianGirls]]);
-      setRemainingGirls((prevState) =>
-        prevState.filter((item) => !armenianGirls.includes(item))
-      );
-      setArmenianSelected(true);
-    } else {
-      const newTeam = [];
-      const remainingGirlsCache = [...remainingGirls];
-      for (let i = 0; i < 3; i++) {
-        const girlIndex = Math.floor(
-          Math.random() * remainingGirlsCache.length
-        );
-        const randomGirl = remainingGirlsCache[girlIndex];
-        remainingGirlsCache.splice(girlIndex, 1);
-        newTeam.push(randomGirl);
-      }
-      setTeams((prevState) => [...prevState, newTeam]);
-      setRemainingGirls([...remainingGirlsCache]);
+  function generateTeams() {
+    const result = [[...armenianGirls]];
+    const remainingGirls = [...ulyanovskGirls];
+    while (remainingGirls.length) {
+      result.push(generateTeam(remainingGirls));
     }
+    setTeams(result);
+  }
+
+  function generateTeam(remainingGirls) {
+    const newTeam = [];
+    for (let i = 0; i < 3; i++) {
+      const girlIndex = Math.floor(Math.random() * remainingGirls.length);
+      const randomGirl = remainingGirls[girlIndex];
+      remainingGirls.splice(girlIndex, 1);
+      newTeam.push(randomGirl);
+    }
+    return newTeam;
   }
 
   return (
     <div className="teams-page">
-      <div className="teams-page__remaining">
-        <div>
-          <h2>Девушки</h2>
-          {remainingGirls.map((girl) => (
-            <div className="teams-page__remaining-item" key={girl}>
-              {girl}
-            </div>
-          ))}
+      {!teams.length ? (
+        <div className="teams-page__remaining">
+          <div>
+            <h2>Девушки</h2>
+            {allGirls.map((girl) => (
+              <div className="teams-page__remaining-item" key={girl}>
+                {girl}
+              </div>
+            ))}
+          </div>
+          <div className="teams-page__button-container">
+            <button className="teams-page__button" onClick={generateTeams}>
+              Собрать
+            </button>
+          </div>
         </div>
-        <div className="teams-page__button-container">
-          <button
-            className="teams-page__button"
-            onClick={generateTeam}
-            disabled={!remainingGirls.length}
-          >
-            Собрать
-          </button>
-          {!remainingGirls.length && (
+      ) : (
+        <div className="teams-page__teams">
+          <div>
+            <h2>Группы</h2>
+            {teams?.map((team, idx) => (
+              <div className="teams-page__teams-list-item" key={idx}>
+                <h3>Группа {idx + 1}</h3>
+                <div>
+                  {team.map((girl, idx) => (
+                    <span key={idx + girl}>
+                      {girl}
+                      {idx < team.length - 1 && ", "}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="teams-page__button-container">
             <Link to="/rebus">
               <button className="teams-page__button">Далее</button>
             </Link>
-          )}
+          </div>
         </div>
-      </div>
-      <div className="teams-page__teams">
-        <div>
-          <h2>Группы</h2>
-          {teams?.map((team, idx) => (
-            <div className="teams-page__teams-list-item" key={idx}>
-              <h3>Группа {idx + 1}</h3>
-              <div>
-                {team.map((girl, idx) => (
-                  <span key={idx + girl}>
-                    {girl}
-                    {idx < team.length - 1 && ", "}
-                  </span>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
