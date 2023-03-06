21313e89eb7e053b6687b663dfbe2c5d123d23b1
Dmitry Umnov <umnov.dm@gmail.com>
2023-03-06 19:46:52 +0400

    hz cho

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,9 +7,9 @@ const HomePage = () => {
 
   return (
     <div className="home-page" onClick={() => navigate("/teams")}>
-      <span className="home-page__title">
+      <h1>
         International <strong>True</strong>Women's Day 2023
-      </span>
+      </h1>
     </div>
   );
 };
--- src/pages/TeamsPage.jsx
+++ src/pages/TeamsPage.jsx
@@ -20,7 +20,7 @@ const allGirls = [...armenianGirls, ...ulyanovskGirls].sort();
 
 const TeamsPage = () => {
   const [remainingGirls, setRemainingGirls] = useState(allGirls);
-  const [teams, setTeams] = useState(null);
+  const [teams, setTeams] = useState([]);
   const [armenianSelected, setArmenianSelected] = useState(false);
 
   function generateTeam() {
@@ -34,11 +34,11 @@ const TeamsPage = () => {
       const newTeam = [];
       const remainingGirlsCache = [...remainingGirls];
       for (let i = 0; i < 3; i++) {
-        const randomIndex = Math.floor(
+        const girlIndex = Math.floor(
           Math.random() * remainingGirlsCache.length
         );
-        const randomGirl = remainingGirlsCache[randomIndex];
-        remainingGirlsCache.splice(randomIndex, 1);
+        const randomGirl = remainingGirlsCache[girlIndex];
+        remainingGirlsCache.splice(girlIndex, 1);
         newTeam.push(randomGirl);
       }
       setTeams((prevState) => [...prevState, newTeam]);
@@ -49,20 +49,35 @@ const TeamsPage = () => {
   return (
     <div className="teams-page">
       <div className="teams-page__remaining">
-        <div className="teams-page__title">Девушки</div>
-        {remainingGirls.map((girl) => (
-          <div className="teams-page__remaining-item" key={girl}>
-            {girl}
-          </div>
-        ))}
+        <div>
+          <h2>Девушки</h2>
+          {remainingGirls.map((girl) => (
+            <div className="teams-page__remaining-item" key={girl}>
+              {girl}
+            </div>
+          ))}
+        </div>
+        <div className="teams-page__button-container">
+          <button
+            className="teams-page__button"
+            onClick={generateTeam}
+            disabled={!remainingGirls.length}
+          >
+            Собрать
+          </button>
+          {!remainingGirls.length && (
+            <Link to="/rebus">
+              <button className="teams-page__button">Далее</button>
+            </Link>
+          )}
+        </div>
       </div>
-
       <div className="teams-page__teams">
         <div>
-          <div className="teams-page__title">Группы</div>
+          <h2>Группы</h2>
           {teams?.map((team, idx) => (
             <div className="teams-page__teams-list-item" key={idx}>
-              <span>Группа {idx + 1}</span>
+              <h3>Группа {idx + 1}</h3>
               <div>
                 {team.map((girl, idx) => (
                   <span key={idx + girl}>
@@ -73,20 +88,6 @@ const TeamsPage = () => {
               </div>
             </div>
           ))}
-        </div>
-        <div className="teams-page__teams-buttons">
-          <button
-            className="teams-page__teams-button"
-            onClick={generateTeam}
-            disabled={!remainingGirls.length}
-          >
-            Собрать
-          </button>
-          {!remainingGirls.length && (
-            <Link to="/rebus">
-              <button className="teams-page__teams-button">Далее</button>
-            </Link>
-          )}
         </div>
       </div>
     </div>
